589ba200339b7b65c69b7590a07430c385cb4f1c
Taylor Broad <taylor@cardinalhorizon.com>
2019-07-19 22:05:51 -0400

    Merge branch 'master' of https://github.com/bossofgames/simvector

--- app/Http/Controllers/AirportController.php
+++ app/Http/Controllers/AirportController.php
@@ -11,7 +11,7 @@ class AirportController extends Controller
     public function getAirport($icao) {
         $airport = DB::connection('mongodb')->collection('airports')->where('gps_code', $icao)->first();
         $charts = DB::connection('mongodb')->collection('charts')->where('icao', $icao)->first();
-        return response()->json(['airport' => $airport, 'charts' => $charts]);
+        return response()->json(['airport' => $airport, 'charts' => $charts, 'cycle' => '1907']);
     }
     public function getDepArr($icao) {
         $dep = DB::connection('mongodb')->collection('flights')->where('dep_icao', $icao)->where('state', 1)->get();
